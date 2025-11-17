664ac3272a7eaf3db029691b54c6086c8dfe3003
Paulo <paulofilhorocha22@gmail.com>
2025-11-17 08:20:08 -0300

    aba-login

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-document.getElementById("btnCalcular").addEventListener("click", function() {
-const peso = parseFloat(document.getElementById("peso").value);
-const altura = parseFloat(document.getElementById("altura").value);
+document.getElementById("btnCalcular").addEventListener("click", function () {
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
 
+    if (peso > 0 && altura > 0) {
+        const imc = (peso / (altura * altura)).toFixed(2);
+        let classificacao = "";
 
-if (peso > 0 && altura > 0) {
-const imc = (peso / (altura * altura)).toFixed(2);
-document.getElementById("resultado").textContent = `Seu IMC é: ${imc}`;
-} else {
-document.getElementById("resultado").textContent = "Preencha os campos corretamente.";
-}
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+        } 
+        else if (imc >= 18.5 && imc < 24.9) {
+            classificacao = "Peso ideal";
+        } 
+        else if (imc >= 25 && imc < 29.9) {
+            classificacao = "Sobrepeso";
+        } 
+        else if (imc >= 30 && imc < 34.9) {
+            classificacao = "Obesidade Grau I";
+        } 
+        else if (imc >= 35 && imc < 39.9) {
+            classificacao = "Obesidade Grau II (severa)";
+        } 
+        else {
+            classificacao = "Obesidade Grau III (mórbida)";
+        }
+
+        document.getElementById("resultado").textContent = `Seu IMC é: ${imc} — ${classificacao}`;
+    } else {
+        document.getElementById("resultado").textContent = "Preencha os campos corretamente.";
+    }
 });
